23cc7433cd6d304179a3ca66102e2045b0525550
Serhiy Slipchuk <slipchuks@ukr.net>
2023-05-22 01:31:27 +0300

    update API

--- src/functions/clientAPI.js
+++ src/functions/clientAPI.js
@@ -1,25 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const getPopularMovies = async function() {
-    const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/day';
-    const searchParams = {
-        api_key: '53cf71a9dc9d51d5f353ddcd73e301a4',
-        language: 'en-US'
+const BASE_URL = 'https://api.themoviedb.org/3';
+const searchParams = {
+  api_key: '53cf71a9dc9d51d5f353ddcd73e301a4',
+  language: 'en-US',
+};
 
-    }
-const response = await axios.get(BASE_URL, {params: searchParams})
-
-return response
-}
+export const getPopularMovies = async function () {
+  const response = await axios.get(`${BASE_URL}/trending/movie/day`, {
+    params: searchParams,
+  });
+  return response;
+};
 
 export const getMoviesBySearchQuerry = async function (query) {
-    const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
-    const searchParams = {
-        query: query,
-        api_key: '53cf71a9dc9d51d5f353ddcd73e301a4',
-        language: 'en-US'
-    }
-
-    const response = await axios.get(BASE_URL, {params: searchParams})
-    return response;
-}
+  const response = await axios.get(`${BASE_URL}/search/movie`, {
+    params: { query, ...searchParams },
+  });
+  return response;
+};
