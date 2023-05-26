d11c9f121a04952ea6cca1a3772ef40a1a588fb8
Serhiy-Slipchuk <114355951+Serhiy-Slipchuk@users.noreply.github.com>
2023-05-26 20:58:06 +0000

    Deploying to gh-pages from @ Serhiy-Slipchuk/goit-react-hw-05-movies@2fbc35072fc84ed4c3ba04fd80149ea1b61eb34c 🚀

---
+++ static/js/126.24d994cf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[126],{9126:function(t,a,e){e.d(a,{KMP:function(){return i}});var r=e(9983);function i(t){return(0,r.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"}}]})(t)}}}]);
+//# sourceMappingURL=126.24d994cf.chunk.js.map
