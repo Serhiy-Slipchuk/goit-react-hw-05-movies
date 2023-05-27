87c1da9af280710ae12dfe94696be020866d5ab0
Serhiy Slipchuk <slipchuks@ukr.net>
2023-05-28 01:13:30 +0300

    update Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,10 +4,14 @@ import Container from 'components/Container/Container';
 import Header from 'components/Header/Header';
 import Navigation from 'components/Navigation/Navigation';
 import menuItems from '../../data/menuItems.json';
+import { Helmet } from 'react-helmet';
 
 const Layout = function () {
   return (
     <>
+    <Helmet>
+    <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
+    </Helmet>
       <Header>
         <Navigation items={menuItems} />
       </Header>
